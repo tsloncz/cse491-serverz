c29a1b5d9ac98c3cbb5775e88b7bd2bec9411111
Timothy Sloncz <slonczti@arctic.cse.msu.edu>
2014-01-10 18:22:59 -0500

    Working hw 1

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 import time
 
 s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
+host = socket.getfqdn()     # Get local machine name
 port = random.randint(8000, 9999)
 s.bind((host, port))        # Bind to the port
 
@@ -18,6 +18,7 @@ while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
+    c.send('HTTP/1.0 200 OK\r\n')       #HTTP 1.0 response
+    c.send("Content-type: text/html\r\n\r\n")   #Header
+    c.send('<h1>Hello, world</h1> this is tsloncz\'s Web server.')
     c.close()
