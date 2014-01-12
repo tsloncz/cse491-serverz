c0e3c0c3a557e426ce79809e50f2a28456c0eb98
tsloncz <tsloncz@gmail.com>
2014-01-12 09:17:54 -0500

    Finalized server.py and updated ChangeLog

--- server.py
+++ server.py
@@ -17,8 +17,10 @@ print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
+    #c.recv(1000) get information about the client
+    print c.recv(1000)
     print 'Got connection from', client_host, client_port
-    c.send('HTTP/1.0 200 OK\r\n')       #HTTP 1.0 response
+    c.send('HTTP/1.0 200 OK\r\n')               #HTTP 1.0 response
     c.send("Content-type: text/html\r\n\r\n")   #Header
     c.send('<h1>Hello, world</h1> this is tsloncz\'s Web server.')
     c.close()
